d530025851f608f1476511d332e9afb342e518bb
Teddy Adien <teddyfikr@gmail.com>
2023-03-22 04:10:31 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,16 +5,16 @@
  *
  */
 
-void print_alphabet_x10(void){
-	int i;
-	int a;
-
-       for(a=0; a<10; a++)
-	{ 
-	      for (i=97; i<123; i++)
-		{
+void print_alphabet_x10(void)
+{
+int i;
+int a;
+for (a = 0 ; a < 10 ; a++)
+{
+	for (i = 97 ; i < 123 ; i++)
+	{
 		_putchar(i);
-                }
 	}
-	_putchar('\n');
+_putchar('\n');
+}
 }
