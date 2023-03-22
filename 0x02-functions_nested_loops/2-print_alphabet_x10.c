ba08d2c14a2e7aecc46710ba4986627ccc568f0e
Teddy Adien <teddyfikr@gmail.com>
2023-03-22 01:45:40 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - function that prints lowercase alphabet ten times
+ *
+ */
+
+void print_alphabet_x10(void){
+	int i;
+	int a;
+
+       for(a=0; a<10; a++)
+	{ 
+	      for (i=97; i<123; i++)
+		{
+		_putchar(i);
+                }
+	}
+	_putchar('\n');
+}
