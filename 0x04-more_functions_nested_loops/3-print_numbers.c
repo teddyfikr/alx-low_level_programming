2bbc6972abbfc603770fe3e9d099db7ec59faafc
Teddy Adien <teddyfikr@gmail.com>
2023-03-27 04:49:54 -0700

    Read me

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_most_numbers -Prints number 0-9 except 2 & 4
+ *
+ * @i: loop variable
+ */
+void print_most_numbers(void)
+{
+	int i = 0;
+
+	for (i = 0 ; i <= 9 ; i++)
+	{
+		_putchar (i);
+	}
+}
