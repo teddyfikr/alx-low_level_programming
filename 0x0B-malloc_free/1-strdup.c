82953bce205c7c9825c9a6c4d3a1e7f557cf639c
Teddy Adien <teddyfikr@gmail.com>
2023-04-11 04:20:55 -0700

    The woman who has no imagination has no wings

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - returns a copy of passed string through malloc
+ *
+ * @str: passed string pointer parameter
+ *
+ * Return: a copy of string pointer
+ */
+char *_strdup(char *str)
+{
+	char *ch;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	ch = strdup(str);
+
+	if (ch == NULL)
+	{
+		return (NULL);
+	}
+	return (ch);
+}
