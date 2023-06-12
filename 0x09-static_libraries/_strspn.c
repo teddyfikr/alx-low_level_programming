b3767119ab02ca5c89b7af9866c60ab3030a6b99
teddyfikr <teddyfikr@gmail.com>
2023-06-11 23:40:08 -0700

    Static Library

--- 0x09-static_libraries/_strspn.c
+++ 0x09-static_libraries/_strspn.c
@@ -1,17 +1,28 @@
 #include "main.h"
-#include <string.h>
 
 /**
-*_strspn - gets the length of a prefix substring
-*@s: string to be checked
-*@accept: string to compare to s
-*
-*Return: number of bytes
-*/
-
+ * _strspn - gets the length of a prefix substring.
+ * @s: initial segment.
+ * @accept: accepted bytes.
+ * Return: the number of accepted bytes.
+ */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i = strspn(s, accept);
+	unsigned int i, j, bool;
 
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		bool = 1;
+		for (j = 0; *(accept + j) != '\0'; j++)
+		{
+			if (*(s + i) == *(accept + j))
+			{
+				bool = 0;
+				break;
+			}
+		}
+		if (bool == 1)
+			break;
+	}
 	return (i);
 }
